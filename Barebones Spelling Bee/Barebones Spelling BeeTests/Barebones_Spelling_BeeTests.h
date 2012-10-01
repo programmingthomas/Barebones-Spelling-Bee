24cefb4f4f693d1084a77597ab38a5a51e224451
Programming Thomas <programmingthomas@gmail.com>
2012-10-01 07:19:05 +0100

    Added license comments to remaining files.

--- Barebones Spelling Bee/Barebones Spelling BeeTests/Barebones_Spelling_BeeTests.h
+++ Barebones Spelling Bee/Barebones Spelling BeeTests/Barebones_Spelling_BeeTests.h
@@ -1,10 +1,20 @@
-//
-//  Barebones_Spelling_BeeTests.h
-//  Barebones Spelling BeeTests
-//
-//  Created by Thomas Denney on 30/09/2012.
-//  Copyright (c) 2012 Programming Thomas. All rights reserved.
-//
+/*
+ Barebones Spelling Bee is a simple iOS app based on flspellingbee.co.uk
+ Copyright (C) 2012  Programming Thomas
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <SenTestingKit/SenTestingKit.h>
 
